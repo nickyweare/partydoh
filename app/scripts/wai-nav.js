50bdc54d1ceaec2d9f33f989d7ccfd04b61da94f
Mark Reynolds <mark@weareinteractive.io>
2017-05-16 17:00:26 +0100

    Tuesday Latest

--- app/scripts/wai-nav.js
+++ app/scripts/wai-nav.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 $(".wai-nav-icon").on('click', function() {
   $('.wai-nav-list').slideToggle(300);
-=======
-$(“.wai-nav-icon”).on(‘click’, function() {
-  $(‘.wai-nav-list’).slideToggle(300);
->>>>>>> b1119ad0dfd0e3f164f5d6b62fa2a265df140523
 });
