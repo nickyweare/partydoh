b7eb4c2ffb3607e18c2e3865d3b46887ea3db868
nickyweare <nicky@weareinteractive.io>
2017-06-02 14:20:29 +0100

    sidebar

--- app/scripts/wai-nav.js
+++ app/scripts/wai-nav.js
@@ -1,3 +1,13 @@
 $('.wai-nav-icon').on('click', function() {
   $('.wai-nav-list').slideToggle(300);
 });
+
+$(window).scroll(function () {
+    var $nav = $('.wai-nav').offset().top;
+    var $packOff = $('.party-includes').offset().top - 90;
+    if ($nav > $packOff) {
+        $('.wai-nav').addClass('nav-hide');
+    } else {
+        $('.wai-nav').removeClass('nav-hide');
+    }
+});
